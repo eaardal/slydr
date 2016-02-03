c92b7143a935bf747da6545b4a8e0f7274240f8c
Eirik Årdal <eiard@statoil.com>
2016-02-03 17:01:03 +0100

    Slide route working

--- Startup.cs
+++ Startup.cs
@@ -19,49 +19,81 @@ namespace Slydr
     {
         private const string ApiToken = "079b22171cd1b3ae67bbab5af21be5880edf05b8";
         private const string ProductIdentifier = "Gittablog";
-         
-        public void ConfigureServices(IServiceCollection services)
-        {
-            
-        }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
+            
+            app.Map("/slide", SlideRoute);
+            
+            app.Run(async (context) =>
+            {
+                var gitHub = CreateGitHubClient();
+                var repositoryContents = await GetRepositoryContent(gitHub);
 
-            app.Run(async (context) => {
-               
-               var gitHubRepositoryOwner = "eaardal";
-               var gitHubRepositoryName = "mdtest";
-               
-               var header = new ProductHeaderValue(ProductIdentifier);
-               var credentialStore = new InMemoryCredentialStore(new Credentials(ApiToken));
-               var github = new GitHubClient(header, credentialStore);
-               
-               var repository = await github.Repository.Get(gitHubRepositoryOwner, gitHubRepositoryName);
-               
-               var content = await github.Repository.Content.GetAllContents(gitHubRepositoryOwner, gitHubRepositoryName, "/");
-              
-               var markdownFiles = content.Where(c => c.Name.EndsWith(".md"));
-               
-               var slides = markdownFiles.Where(md => md.Name.StartsWith("slide"));
-               
-               foreach (var slide in slides) 
-               {
-                   var slideContent = await Download(slide.DownloadUrl);
-                    
-                   var slideMarkdown = new Markdown().Transform(slideContent);
-                   
-                   await context.Response.WriteAsync(slideMarkdown);
-               }                              
+                var nrOfSlides = repositoryContents.Count(c => c.Name.EndsWith(".md") && c.Name.StartsWith("slide"));
+
+                await Write(context, "<p>Found " + nrOfSlides + " slides</p>");
+                await Write(context, "<a href='http://localhost:5000/slide/?nr=1'>Start</a>");
             });
         }
-        
-        private async Task Log(HttpContext context, string message)
+
+        private void SlideRoute(IApplicationBuilder app)
         {
-            await context.Response.WriteAsync(message + "\n");
+            app.Run(async (context) =>
+            {
+                var slideNr = GetCurrentSlideNr(context);
+                var slideHtml = await GetSlide(slideNr);
+
+                await Write(context, slideHtml);
+
+                await context.Response.WriteAsync("<a href='http://localhost:5000/slide/?nr=" + (slideNr + 1) + "'>Next slide</a>");
+            });
         }
-        
+
+        private int GetCurrentSlideNr(HttpContext context)
+        {
+            return context.Request.Query.ContainsKey("nr") ? int.Parse(context.Request.Query.First(q => q.Key == "nr").Value.First()) : 1;
+        }
+
+        private async Task<string> GetSlide(int slideNr)
+        {
+            var gitHub = CreateGitHubClient();
+            var content = await GetRepositoryContent(gitHub);
+
+            var markdownFiles = content.Where(c => c.Name.EndsWith(".md"));
+
+            var slide = markdownFiles.Where(md => md.Name.StartsWith("slide") && md.Name.Contains("0" + slideNr)).FirstOrDefault();
+
+            if (slide != null)
+            {
+                var slideMarkdown = await Download(slide.DownloadUrl);
+                return new Markdown().Transform(slideMarkdown);
+            }
+            return string.Empty;
+        }
+
+        private GitHubClient CreateGitHubClient()
+        {
+            var header = new ProductHeaderValue(ProductIdentifier);
+            var credentialStore = new InMemoryCredentialStore(new Credentials(ApiToken));
+            return new GitHubClient(header, credentialStore);
+        }
+
+        private async Task<IReadOnlyList<RepositoryContent>> GetRepositoryContent(GitHubClient github)
+        {
+            const string gitHubRepositoryOwner = "eaardal";
+            const string gitHubRepositoryName = "mdtest";
+
+            var repository = await github.Repository.Get(gitHubRepositoryOwner, gitHubRepositoryName);
+            return await github.Repository.Content.GetAllContents(gitHubRepositoryOwner, gitHubRepositoryName, "/");
+        }
+
+        private async Task Write(HttpContext context, string message)
+        {
+            await context.Response.WriteAsync(message + "<br/>");
+        }
+
         private async Task<string> Download(Uri url)
         {
             var webClient = new WebClient();
