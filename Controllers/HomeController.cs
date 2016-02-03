a317eee9824a8ca60ca905c45d880b1b09ab743c
Eirik Årdal <eaardal@outlook.com>
2016-02-03 20:39:57 +0100

    Added HomeController, moved prototype logic into temp class and out of Startup

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using MarkdownSharp;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+using Octokit;
+
+namespace Slydr
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Slide()
+        {
+            return null;
+        }
+    }
+
+    class Temp : Controller
+    {
+        private async Task Default()
+        {
+            try
+            {
+                var repositoryOwner = GetRepositoryOwner();
+                var repositoryName = GetRepositoryName();
+                var gitHub = CreateGitHubClient();
+                var repositoryContents = await GetRepositoryContent(gitHub, repositoryOwner, repositoryName);
+
+                var nrOfSlides = repositoryContents.Count(c => c.Name.EndsWith(".md") && c.Name.StartsWith("slide"));
+
+                var url = CreateNextSlideUrl(1);
+                //await context.Response.WriteAsync("<a href='" + url + "'>Start</a>");
+            }
+            catch (Exception)
+            {
+                //context.Response.Clear();
+                //await context.Response.WriteAsync("Some shit went wrong :(");
+            }
+        }
+
+        private async Task SlideRoute()
+        {
+            try
+            {
+                var slideNr = GetCurrentSlideNr();
+                var slideHtml = await GetSlide(slideNr);
+
+                //await Write(context, slideHtml);
+
+                var url = CreateNextSlideUrl(slideNr + 1);
+                //await context.Response.WriteAsync("<a href='" + url + "'>Next slide</a>");
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private string CreateNextSlideUrl(int slideNr)
+        {
+            var repositoryOwner = GetRepositoryOwner();
+            var repositoryName = GetRepositoryName();
+            return "http://localhost:5000/slide/?nr=" + slideNr + "&owner=" + repositoryOwner + "&repo=" + repositoryName;
+        }
+
+        private int GetCurrentSlideNr()
+        {
+            return HttpContext.Request.Query.ContainsKey("nr")
+                ? int.Parse(HttpContext.Request.Query["nr"])
+                : 1;
+        }
+
+        private async Task<string> GetSlide(int slideNr)
+        {
+            var repositoryOwner = GetRepositoryOwner();
+            var repositoryName = GetRepositoryName();
+            var gitHub = CreateGitHubClient();
+            var content = await GetRepositoryContent(gitHub, repositoryOwner, repositoryName);
+
+            var markdownFiles = content.Where(c => c.Name.EndsWith(".md"));
+
+            var slide = markdownFiles.Where(md => md.Name.StartsWith("slide") && md.Name.Contains("0" + slideNr)).FirstOrDefault();
+
+            if (slide != null)
+            {
+                var slideMarkdown = await Download(slide.DownloadUrl);
+                return new Markdown().Transform(slideMarkdown);
+            }
+            return string.Empty;
+        }
+
+        private string GetRepositoryName()
+        {
+            if (!HttpContext.Request.Query.ContainsKey("repo"))
+            {
+                throw new Exception("No repository name given as query string");
+            }
+            return HttpContext.Request.Query["repo"];
+        }
+
+        private string GetRepositoryOwner()
+        {
+            if (!HttpContext.Request.Query.ContainsKey("owner"))
+            {
+                throw new Exception("No repository owner given as query string");
+            }
+            return HttpContext.Request.Query["owner"];
+        }
+
+        private GitHubClient CreateGitHubClient()
+        {
+            var configuration = (IConfiguration)HttpContext.RequestServices.GetService(typeof(IConfiguration));
+            var productIdentifier = configuration["github.productIdentifier"];
+            var header = new ProductHeaderValue(productIdentifier);
+            return new GitHubClient(header);
+        }
+
+        private async Task<IReadOnlyList<RepositoryContent>> GetRepositoryContent(GitHubClient github, string repositoryOwner, string repositoryName)
+        {
+            var repository = await github.Repository.Get(repositoryOwner, repositoryName);
+            return await github.Repository.Content.GetAllContents(repositoryOwner, repositoryName, "/");
+        }
+
+        private async Task<string> Download(Uri url)
+        {
+            var webClient = new WebClient();
+            using (var stream = new MemoryStream(await webClient.DownloadDataTaskAsync(url)))
+            {
+                stream.Position = 0;
+                var reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+            }
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
-using MarkdownSharp;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Octokit;
-using Octokit.Internal;
+using Newtonsoft.Json.Serialization;
 using Serilog;
 using Serilog.Events;
 
@@ -46,6 +37,7 @@ namespace Slydr
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging();
+            services.AddMvc().AddJsonOptions(o => o.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
         }
 
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
@@ -55,132 +47,9 @@ namespace Slydr
 
             app.UseIISPlatformHandler();
 
-            app.Map("/slide", SlideRoute);
-
-            app.Run(async (context) =>
-            {
-                try
-                {
-                    var repositoryOwner = GetRepositoryOwner(context);
-                    var repositoryName = GetRepositoryName(context);
-                    var gitHub = CreateGitHubClient();
-                    var repositoryContents = await GetRepositoryContent(gitHub, repositoryOwner, repositoryName);
-
-                    var nrOfSlides = repositoryContents.Count(c => c.Name.EndsWith(".md") && c.Name.StartsWith("slide"));
-
-                    await Write(context, "<p>Found " + nrOfSlides + " slides</p>");
-                    
-                    var url = CreateNextSlideUrl(1, context);
-                    await context.Response.WriteAsync("<a href='" + url + "'>Start</a>");
-                }
-                catch (Exception)
-                {
-                    context.Response.Clear();
-                    await context.Response.WriteAsync("Some shit went wrong :(");
-                }
-            });
+            app.UseMvc();
         }
-
-        private void SlideRoute(IApplicationBuilder app)
-        {
-            app.Run(async (context) =>
-            {
-                try
-                {
-                    var slideNr = GetCurrentSlideNr(context);
-                    var slideHtml = await GetSlide(slideNr, context);
-
-                    await Write(context, slideHtml);
-
-                    var url = CreateNextSlideUrl(slideNr + 1, context);
-                    await context.Response.WriteAsync("<a href='" + url + "'>Next slide</a>");
-                }
-                catch (Exception)
-                {
-                    context.Response.Clear();
-                    await context.Response.WriteAsync("Some shit went wrong :(");
-                }
-            });
-        }
-
-        private string CreateNextSlideUrl(int slideNr, HttpContext context)
-        {
-            var repositoryOwner = GetRepositoryOwner(context);
-            var repositoryName = GetRepositoryName(context);
-            return "http://localhost:5000/slide/?nr=" + slideNr + "&owner=" + repositoryOwner + "&repo=" + repositoryName;
-        }
-
-        private int GetCurrentSlideNr(HttpContext context)
-        {
-            return context.Request.Query.ContainsKey("nr") ? int.Parse(context.Request.Query.First(q => q.Key == "nr").Value.First()) : 1;
-        }
-
-        private async Task<string> GetSlide(int slideNr, HttpContext context)
-        {
-            var repositoryOwner = GetRepositoryOwner(context);
-            var repositoryName = GetRepositoryName(context);
-            var gitHub = CreateGitHubClient();
-            var content = await GetRepositoryContent(gitHub, repositoryOwner, repositoryName);
-
-            var markdownFiles = content.Where(c => c.Name.EndsWith(".md"));
-
-            var slide = markdownFiles.Where(md => md.Name.StartsWith("slide") && md.Name.Contains("0" + slideNr)).FirstOrDefault();
-
-            if (slide != null)
-            {
-                var slideMarkdown = await Download(slide.DownloadUrl);
-                return new Markdown().Transform(slideMarkdown);
-            }
-            return string.Empty;
-        }
-
-        private string GetRepositoryName(HttpContext context)
-        {
-            if (!context.Request.Query.ContainsKey("repo"))
-            {
-                throw new Exception("No repository name given as query string");
-            }
-            return context.Request.Query["repo"];
-        }
-
-        private string GetRepositoryOwner(HttpContext context)
-        {
-            if (!context.Request.Query.ContainsKey("owner"))
-            {
-                throw new Exception("No repository owner given as query string");
-            }
-            return context.Request.Query["owner"];
-        }
-
-        private GitHubClient CreateGitHubClient()
-        {
-            var productIdentifier = Configuration["github.productIdentifier"];
-            var header = new ProductHeaderValue(productIdentifier);
-            return new GitHubClient(header);
-        }
-
-        private async Task<IReadOnlyList<RepositoryContent>> GetRepositoryContent(GitHubClient github, string repositoryOwner, string repositoryName)
-        {
-            var repository = await github.Repository.Get(repositoryOwner, repositoryName);
-            return await github.Repository.Content.GetAllContents(repositoryOwner, repositoryName, "/");
-        }
-
-        private async Task Write(HttpContext context, string message)
-        {
-            await context.Response.WriteAsync(message + "<br/>");
-        }
-
-        private async Task<string> Download(Uri url)
-        {
-            var webClient = new WebClient();
-            using (var stream = new MemoryStream(await webClient.DownloadDataTaskAsync(url)))
-            {
-                stream.Position = 0;
-                var reader = new StreamReader(stream);
-                return reader.ReadToEnd();
-            }
-        }
-
+        
         public static void Main(string[] args)
         {
             Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
